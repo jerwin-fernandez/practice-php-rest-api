6e80f30ae6caf9793bc1d55a81d8ffcea3daa570
Jerwin <jerwinfernandez04@gmail.com>
2020-03-02 00:42:22 +0800

    completed single read and create api

--- models/Post.php
+++ models/Post.php
@@ -104,14 +104,24 @@
       // striptags
       $this->title = htmlspecialchars(strip_tags($this->title));
       $this->body = htmlspecialchars(strip_tags($this->body));
-      $this->author = htmlspecialchars(strip_tags($this->title));
-      $this->category_id = htmlspecialchars(strip_tags($this->title));
+      $this->author = htmlspecialchars(strip_tags($this->author));
+      $this->category_id = htmlspecialchars(strip_tags($this->category_id));
 
       // bind data
       $stmt->bindParam(':title', $this->title);
       $stmt->bindParam(':body', $this->body);
       $stmt->bindParam(':author', $this->author);
       $stmt->bindParam(':category_id', $this->category_id);
+
+      // execute query
+      if($stmt->execute()){
+        return true;
+      }
+
+      // print error if something goes wrong
+      printf("Error: %s.\n", $stmt->error);
+      return false;
+    
     }
 
   }
